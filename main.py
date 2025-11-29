d61d46b4180d1faacf3bfdf5257c72d1a9fe6289
xjavoxir2197 <xjavoxir2197@gmail.com>
2025-11-29 17:40:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# 11
+a_harfi = lambda name: 'a' in name.lower()
+print(a_harfi('zuhra'))
+print(a_harfi('dilobar'))
+
+# 12
+hrflar = lambda s: s.upper()
+print(hrflar('salom dunyo'))
+
+# 13
+roy = lambda a, b: abs(a - b)
+print(roy(4, 10))
+print(roy(10,4))
+
+# 14
+qiyoslash = lambda a, b: a == b
+print(qiyoslash(5, 12))
+print(qiyoslash(8, 4))
+
+# 15
+bolish_2 =  lambda x: x // 2
+print(bolish_2(8))
+print(bolish_2(10))
